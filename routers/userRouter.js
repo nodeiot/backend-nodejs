27f754f085a867f689e07c95851150efccc13095
Bi3la <onegabriel2009@gmail.com>
2025-07-23 22:02:27 -0300

    Implementação do mongodb e da geração de token

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,30 +1,47 @@
-const { Router }  = require('express') //importa a funçã oda biblioteca express
-const expressAsyncHandler = require ('express-async-handler');
+const { Router } = require('express') //importa a função da biblioteca express
+const expressAsyncHandler = require('express-async-handler');
+const User = require('../models/userModel.js');
+const { generateToken } = require ('../utils.js');
 
 const userRouter = Router();
-let users = []
 
-userRouter.post('/register', expressAsyncHandler (async (req, res) =>{
-    const { email, password, passwordConfirm, name } = req.body
-    if(!email || !password) return res.status(400).send({message: `Email e senha são obrigatórios`})
-    if (users.some(item=> item.email == email)) return res.status(400).send({ message: 'Email já cadastrado' })
-    if(!email.includes('@')) return res.status(400).send({ message: 'Email não é válido'})
-    if(password != passwordConfirm) return res.status(400).send({ message: 'As duas senhas são diferentes' })
-    users.push({ email, password, name })
-    return res.status(200).send({message: `Usuário com e-mail: ${email} criado com sucesso` })
+userRouter.post('/register', expressAsyncHandler(async (req, res) => {
+
+    try {
+        const { email, password, passwordConfirm, name } = req.body
+        if (!email || !password) return res.status(400).send({ message: `Email e senha são obrigatórios` })
+
+        if (!email.includes('@')) return res.status(400).send({ message: 'Email não é válido' })
+        if (password != passwordConfirm) return res.status(400).send({ message: 'As duas senhas são diferentes' })
+        const user = await User.create({
+            email,
+            password,
+            name
+        })
+        console.log(user)
+        return res.status(200).send({ message: `Usuário ${user.name} criado com sucesso` })
+    } catch (error) {
+        return res.status(400).send(error)
+    }
 }))
 
-userRouter.post ('/login', expressAsyncHandler(async (req, res) => {
-    const { email, password } = req.body
+userRouter.post('/login', expressAsyncHandler(async (req, res) => {
+    try {
+        const { email, password } = req.body
 
-    if (!email || !password) return res.status(400).send({ message: 'Email e senha são obrigatórios' })
-    const user = users.find(item => item.email == email)
-    
-    if(!user) return res.status(400).send({ message: 'Usuário não encontrado'})
-    if(user.password != password) return res.status(400).send({message: 'Senha inválida' })
-    
-    const { password : passwordRemoved, ...result} = user 
-    return res.status(200).send({ message: 'Usuário logado com sucesso!', user: result })
+        if (!email || !password) return res.status(400).send({ message: 'Email e senha são obrigatórios' })
+
+        const user = await User.findOne({ email: email }).lean()
+
+        if (!user) return res.status(400).send({ message: 'Usuário não encontrado' })
+        if (user.password != password) return res.status(400).send({ message: 'Senha inválida' })
+
+        const { password: passwordRemoved, ...result } = user
+        const token = generateToken (user)
+        return res.status(200).send({ user: {...result, token}, message: 'Usuário logado com sucesso!'})
+    } catch (error) {
+        return res.status(400).send(error)
+    }
 }))
 
 module.exports = userRouter
