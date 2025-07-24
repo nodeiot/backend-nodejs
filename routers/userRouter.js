b81e1133fdca29727f46444073921ed564db0c54
Bi3la <onegabriel2009@gmail.com>
2025-07-24 20:58:28 -0300

    Criação do update de nome do usuário

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,7 @@
 const { Router } = require('express') //importa a função da biblioteca express
 const expressAsyncHandler = require('express-async-handler');
 const User = require('../models/userModel.js');
-const { generateToken } = require ('../utils.js');
+const { generateToken, isAuth } = require ('../utils.js');
 
 const userRouter = Router();
 
@@ -37,10 +37,27 @@ userRouter.post('/login', expressAsyncHandler(async (req, res) => {
         if (user.password != password) return res.status(400).send({ message: 'Senha inválida' })
 
         const { password: passwordRemoved, ...result } = user
+
         const token = generateToken (user)
+
         return res.status(200).send({ user: {...result, token}, message: 'Usuário logado com sucesso!'})
     } catch (error) {
         return res.status(400).send(error)
+    }
+}))
+
+userRouter.post('/update', isAuth, expressAsyncHandler(async (req, res) => {
+    try {
+        const { _id} = req.user
+        const { newName } = req.body
+        const user = await User.findById(_id)
+        user.name = newName
+        const userUpdated = await user.save()
+
+        console.log(_id, newName)
+        return res.status(200).send({ message: "Usuário atualizado com sucesso!"})
+    } catch {
+        return res.status(400).send({ message: error})
     }
 }))
 
