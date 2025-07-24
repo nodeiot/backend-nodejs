3d2a7a7b90ccf9aa4440de41619ab751fc54e0c9
JonathanJusto <jonathanjusto@gmail.com>
2025-07-23 22:02:31 -0300

    mongodb

---
+++ utils.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const segredo = "naocontapraninguem"
+
+const generateToken = (user) => {
+    const token = jwt.sign({
+        _id: user._id,
+        email: user.email
+    }, segredo,
+        {
+            expiresIn: '1d'
+        }
+    )
+};
+module.exports = {generateToken};
