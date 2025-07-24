ab9d281975f087e7acb178f02951783ceb4e0a74
Rafael <rafael.rebelo47@gmail.com>
2025-07-23 22:02:52 -0300

    Conectando ao banco de dados e criando token

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,28 +1,54 @@
 const { Router } = require('express')
 const expressAsyncHandler = require('express-async-handler');
+const User = require('../models/userModel')
+const {generateToken} = require('../utils');
 
 const userRouter = Router();
-let users = []
+
 
 userRouter.post('/register', expressAsyncHandler(async (req, res)=> {
-    const { email, password, passwordConfirm, name } = req.body
-    if(!email || !password) return res.status(400).send({message: 'Email e senha inválidos, tente novamente'})
-    if(!email.includes('@')) return res.status(400).send({message: 'Email não é válido'})
-    if(password != passwordConfirm) return res.status(400).send({message: 'Senhas estão diferentes, tente novamente.'})
-    if (users.some(item=> item.email == email)) return res.status(400).send({message: 'Email já está cadastrado.'})
-    console.log ({email, password})
-    users.push({ email, password })
-    return res.status(200).send({message: `Usuário com email: ${email} criado com sucesso`})
+
+    try{
+        const { email, password, passwordConfirm, name } = req.body
+        if(!email || !password) return res.status(400).send({message: 'Email e senha inválidos, tente novamente'})
+
+        if(!email.includes('@')) return res.status(400).send({message: 'Email não é válido'})
+
+        if(password != passwordConfirm) return res.status(400).send({message: 'Senhas estão diferentes, tente novamente.'})
+
+        const user = await User.create({
+            email, 
+            password, 
+            name
+        })
+
+        console.log(user)
+        return res.status(200).send({message: `Usuário ${user.name} criado com sucesso`})
+
+}   catch (error) {
+        return res.status(400).send(error)
+}
+    
 }))
 
 userRouter.post('/login' , expressAsyncHandler(async (req, res)=>{
+    try{
     const { email, password} = req.body
+
     if (!email || !password) return res.status(400).send({ message: 'Email e senha são obrigatórios'})
-    const user = user.find(item => item.email == email)
+
+    const user = await User.findOne({ email: email }).lean()
+
     if (!user) return res.status(400).send({message: 'Usuário não encontrado'})
+
     if(user.password != password) return res.status(400).send({message: "Senha inválida"})
+
     const { password: passwordRemoved, ...result } = user
-    return res.status(200).send({user:user, message: 'Login realizado com sucesso'})
+    const token = generateToken(user)
+    return res.status(200).send({user:{result,token}, message: 'Login realizado com sucesso'})
+} catch (error) {
+        return res.status(400).send(error)
+}
 }))
 
 module.exports = userRouter 
---
+++ utils.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+const segredo = "naocontapraninguem"
+
+const generateToken = (user) => {
+    return jwt.sign({
+        _id: user._id,
+        email: user.email        
+
+    }, segredo,
+        {
+            expiresIn: '1d'
+        }
+    
+    )
+}
+
+module.exports = { generateToken }
