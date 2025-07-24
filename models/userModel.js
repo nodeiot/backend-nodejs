2a0ba46ed13f1330a5bdc3ad492f43399b0f1fb5
AlessandraDemarchiRox <alessandra.demarchi@roxcode.dev>
2025-07-23 22:01:28 -0300

    connect to mongo and generateToken

---
+++ models/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const userSchema  = new Schema(
+    {
+        email: {type: String, require: true, unique:  true},
+        password: {type: String, require: true},
+        name: {type: String},
+    },
+    
+    {timestamps: true,}
+)
+
+module.exports = mongoose.model('user', userSchema);
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,43 +1,74 @@
-const Router = require('express');
-const expressAsyncHandler = require('express-async-handler');
+const Router = require("express");
+const expressAsyncHandler = require("express-async-handler");
+
+const userSchema = require("../models/userModel");
+const { generateToken } = require("../utils");
 
 const userRouter = Router();
 
-let users = []
-userRouter.post('/register', expressAsyncHandler( async (req, res) =>{
-    const {email, password, confirmPassword, name} = req.body;
-    if(!email || !password || !confirmPassword){
-        return res.status(400).send({message: 'E-mail e senha são obrigatorios'})
-    }
-    if(password != confirmPassword){
-        return res.status(400).send({message: 'As senhas não são iguais'})
-    }
-    if(!email.includes('@')){
-        return res.status(400).send({message: 'E-mail invalido'})
-    }
-    if(users.some(item => item.email == email)){
-        return res.status(400).send({message: 'E-mail já cadastrado'})
-    }
-    users.push({email, password})
-    console.log(users);
-    return res.status(200).send({message: `Usuário com e-mail ${email} criado com sucesso`})
-}))
+let users = [];
+userRouter.post(
+  "/register",
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const { email, password, confirmPassword, name } = req.body;
+      if (!email || !password) {
+        return res
+          .status(400)
+          .send({ message: "E-mail e senha são obrigatorios" });
+      }
+      if (!email.includes("@")) {
+        return res.status(400).send({ message: "E-mail invalido" });
+      }
 
+      const existingUser = await userSchema.findOne({ email: email});
+      if (existingUser) {
+          return res.status(400).send({ message: 'E-mail já cadastrado' });
+      }
 
-userRouter.post('/login', expressAsyncHandler( async (req, res) =>{
-   const {email, password} = req.body;
-   if(!email || !password){
-        return res.status(400).send({message: 'E-mail e senha são obrigatorios!'})
-    }
-    const user = users.find(item => item.email == email)
-    if(!user){
-        return res.status(400).send({message: 'Usuário não cadastrado!'})
-    }
-    if(user.password != password){
-        return res.status(400).send({message: 'Senha invalida!'})
-    }
-     const {password: passwordToDelete, ...result} = user
-    return res.status(200).send({user: result, message: 'Login realizado com sucesso!'})
-}))
+      const user = await userSchema.create({
+        email,
+        password,
+        name,
+      });
 
-module.exports = userRouter
+      return res
+        .status(200)
+        .send({ message: `Usuário ${user.name} criado com sucesso` });
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  })
+);
+
+userRouter.post(
+  "/login",
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      if (!email || !password) {
+        return res
+          .status(400)
+          .send({ message: "E-mail e senha são obrigatorios!" });
+      }
+      const user = await userSchema.findOne({ email: email }).lean();
+      console.log(user);
+      if (!user) {
+        return res.status(400).send({ message: "Usuário não cadastrado!" });
+      }
+
+      if (user.password != password) {
+        return res.status(400).send({ message: "Senha invalida!" });
+      }
+      const { password: passwordToDelete, ...result } = user;
+      const token =  generateToken(user)
+      return res
+        .status(200)
+        .send({ user: {...result, token}, message: "Login realizado com sucesso!" });
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  })
+);
+
+module.exports = userRouter;
