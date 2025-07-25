b5fb16681297771dcaefe6ea455933d400a2e4a6
BrayanFragaDev <probrayanfraga@gmail.com>
2025-07-25 16:33:32 -0300

    Ajuste na lógica de update e get

--- routers/gamesRouter.js
+++ routers/gamesRouter.js
@@ -7,19 +7,50 @@ const gamesRouter = Router();
 
 gamesRouter.post(
   "/update",
+  isAuth,
   expressAsyncHandler(async (req, res) => {
     try {
-      const { userId } = req.user;
-      const { newGame } = req.body;
-      const user = await User.findById(userId);
-      user.game = newGame;
-      const userUpdated = await user.save();
-      return res
-        .status(200)
-        .send({ message: "Jogos favoritos atualizados com sucesso" });
+      const userId = req.user._id;
+      const { games } = req.body; // jogos deve ser um array de objetos com titulo, anolancamento, desenvolvedora sem qualquer informação vai quebrar
+
+      if (!Array.isArray(games) || games.length === 0) {
+        return res.status(400).send({ message: "Lista de jogos inválida, já estão cadastrados." });
+      }
+
+      // Remove todos os jogos antigos do usuário
+      await Games.deleteMany({ userId });
+
+      // Insere os novos jogos
+      const newGames = games.map((game) => ({
+        userId,
+        titulo: game.titulo,
+        anolancamento: game.anolancamento,
+        desenvolvedora: game.desenvolvedora,
+      }));
+
+      await Games.insertMany(newGames);
+
+      return res.status(200).send({ message: "Jogos atualizados com sucesso!" });
+
     } catch (error) {
-      return res.status(400).send({ message: error });
+      return res.status(400).send({ message: error })
     }
   })
 );
+
+gamesRouter.get("/mygames", isAuth, expressAsyncHandler(async (req, res) => {
+    try {
+      const userId = req.user._id;
+
+      const games = await Games.find({ userId });
+
+      return res.status(200).send(games);
+
+    } catch (error) {
+
+      return res.status(400).send({ message: error })
+    }
+  })
+);
+
 module.exports = gamesRouter;
