44d047030039093e8c6bbb315dac20494bb88ece
clenz-niot <christofer.lenz@nodeiot.com.br>
2025-07-25 20:25:29 -0300

    merge music and games

---
+++ models/gamesModel.js
@@ -0,0 +1,19 @@
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GamesSchema = new Schema({
+
+    userId: { type: Schema.Types.ObjectId, ref: 'User' },
+    titulo: { type: String, require: true, unique: true },
+    anolancamento: { type: String, require: true },
+    desenvolvedora: { type: String }
+
+}, {
+
+    timestamps: true,
+}
+);
+
+module.exports = mongoose.model("Games", GamesSchema);
